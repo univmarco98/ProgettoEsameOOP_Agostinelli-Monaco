6dabbd6a0c09dfce7585d3354e2b9d524a0f3a95
Marco Agostinelli <s1081890@studenti.univpm.it>
2021-01-12 11:54:07 +0100

    Consegna Progetto
    Consegna finale

--- Progetto_EsameOOP_AgostinelliMonaco/src/main/java/application/ProgettoEsameOopAM_Main.java
+++ Progetto_EsameOOP_AgostinelliMonaco/src/main/java/application/ProgettoEsameOopAM_Main.java
@@ -12,9 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProgettoEsameOopAM_Main {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ProgettoEsameOopAM_Main.class, args);
-		
 	}
 
 }
